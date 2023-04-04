dea3da3ba400571fd53174fd4a910b8f644e1bde
sav <christopher.savini@gmail.com>
2023-04-04 19:04:01 +1000

    documentation notes

--- Scripts/MoveMod.ps1
+++ Scripts/MoveMod.ps1
@@ -1,1 +1,7 @@
 Copy-Item -Path "..\Mod\bin\PeepMod.SC2Mod" -Destination "D:\Program Files (x86)\StarCraft II\Mods\PeepMod.SC2Mod"
+
+cd "D:\Program Files (x86)\StarCraft II\Mods\scion-keiron-dev"
+git pull
+
+
+Also check use latest
