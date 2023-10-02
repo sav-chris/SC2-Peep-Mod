05758124b1647b03b8b9ab1d6d19cfabc677f23d
Sav <christopher.savini@gmail.com>
2023-10-02 13:12:06 +1100

    Use C drive since this is the default for most systems and what I use now

--- Scripts/MoveMod.ps1
+++ Scripts/MoveMod.ps1
@@ -1,1 +1,1 @@
-Copy-Item -Path "..\Mod\bin\PeepMod.SC2Mod" -Destination "D:\Program Files (x86)\StarCraft II\Mods\PeepMod.SC2Mod"
+Copy-Item -Path "..\Mod\bin\PeepMod.SC2Mod" -Destination "C:\Program Files (x86)\StarCraft II\Mods\PeepMod.SC2Mod"
